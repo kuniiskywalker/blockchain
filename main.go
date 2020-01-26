dba1d71d1cb9c5c26861a71814111a525ac62522
國井弘一 <kuniiskywalker@gmail.com>
2020-01-27 01:27:32 +0900

    add verify transaction and gofmt

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "blockchain/wallet"
-    "log"
+	"blockchain/block"
+	"blockchain/wallet"
+	"fmt"
+	"log"
 )
 
 func init() {
-    log.SetPrefix("Blockchain: ")
+	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-    w := wallet.NewWallet()
-    fmt.Println(w.PrivateKeyStr())
-    fmt.Println(w.PublicKeyStr())
-    fmt.Println(w.BlockchainAddress())
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
 
-    t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
-    fmt.Printf("signature %s \n", t.GenerateSignature())
+	// Wallet
+	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+
+	// Blockchain
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0, walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added?", isAdded)
+
+	// Mining
+	blockchain.Mining()
+	blockchain.Print()
+
+	fmt.Printf("A %.1f\n", blockchain.CaluculateTotalAmount(walletA.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", blockchain.CaluculateTotalAmount(walletB.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", blockchain.CaluculateTotalAmount(walletM.BlockchainAddress()))
 }
-
-
---
+++ utils/ecdsa.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"fmt"
+	"math/big"
+)
+
+type Signature struct {
+	R *big.Int
+	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%s%s", s.R, s.S)
+}
